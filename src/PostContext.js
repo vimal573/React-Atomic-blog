a068a6a8fdc915d38c6cdc2d2a32b9bdba07f03f
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-18 19:13:24 +0530

    Bug Fixes

--- src/PostContext.js
+++ src/PostContext.js
@@ -51,6 +51,9 @@ function PostProvider({ children }) {
 
 function usePosts() {
   const context = useContext(PostContext);
+  if (context === undefined)
+    throw new Error('PostContext was used outside of the PostProvider');
+
   return context;
 }
 
