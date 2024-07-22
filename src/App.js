4720b36401c697fb6c6932ae1ad859fa31fd6c66
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-22 15:54:02 +0530

    Optimizing Context Re-renders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useContext, useEffect, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { faker } from '@faker-js/faker';
 import { PostProvider, usePosts } from './PostContext';
 
@@ -140,14 +140,14 @@ function List() {
   );
 }
 
-function Archive() {
+const Archive = memo(function Archive() {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
 
   const { onAddPost } = usePosts();
 
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
-    Array.from({ length: 10000 }, () => createRandomPost())
+    Array.from({ length: 1000 }, () => createRandomPost())
   );
 
   const [showArchive, setShowArchive] = useState(false);
@@ -173,7 +173,7 @@ function Archive() {
       )}
     </aside>
   );
-}
+});
 
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
